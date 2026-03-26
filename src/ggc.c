231652b7055bca7e209743d61f4867021c6d372b
Gilgamesh64 <chto24io@gmail.com>
2026-03-26 17:39:34 +0100

    avoid sanitizing by gc sweep

--- src/ggc.c
+++ src/ggc.c
@@ -209,6 +209,11 @@ static void coalesce(block_t *block) {
     }
 }
 
+static void gc_free_no_sanitize(block_t* block){
+    block->free = true;
+    coalesce(block);
+}
+
 /** Returns memory back to the allocator
   * It does NOT eliminate the contents of the memory chunk until it is over written
   * using a freed pointer will work unless the allocator writes something over the memory chunk
@@ -226,8 +231,7 @@ bool gc_free(const void *ptr) {
         if(gc_debug) printf("EROOOR TRIED TO FREE RANDOM ASS POINTER! \nPointer %p was not allocated via gc_malloc() \nYOU STOOPID\n\n", ptr);
         return false;
     }
-    block->free = true;
-    coalesce(block);
+    gc_free_no_sanitize(block);
     return true;
 }
 
@@ -285,7 +289,7 @@ static void gc_sweep(){
                 printf("SWEEPING:\n");
                 print_block(current);
             }
-            gc_free(current + 1);
+            gc_free_no_sanitize(current);
         }
         current -> marked = false;
         current = current -> next;
